35f1a5b49e40612238a7cdf0c8817aa80876d9da
yash <yashpk789987@gmail.com>
2020-02-24 00:44:20 +0530

    messaging bugs fixed

--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -20,22 +20,41 @@ const newChannelMessageSubscription = gql`
 
 class MessageContainer extends React.Component {
   componentWillMount() {
+    this.unsubscribe = this.subscribe(this.props.channelId);
+  }
+
+  componentWillReceiveProps({ channelId }) {
+    if (this.props.channelId !== channelId) {
+      if (this.unsubscribe) {
+        this.unsubscribe();
+      }
+      this.unsubscribe = this.subscribe(channelId);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+  }
+
+  subscribe = channelId =>
     this.props.data.subscribeToMore({
       document: newChannelMessageSubscription,
       variables: {
-        channelId: this.props.channelId
+        channelId
       },
       updateQuery: (prev, { subscriptionData }) => {
         if (!subscriptionData) {
           return prev;
         }
+
         return {
           ...prev,
           messages: [...prev.messages, subscriptionData.data.newChannelMessage]
         };
       }
     });
-  }
 
   render() {
     const {
@@ -80,5 +99,8 @@ const messagesQuery = gql`
 export default graphql(messagesQuery, {
   variables: props => ({
     channelId: props.channelId
-  })
+  }),
+  options: {
+    fetchPolicy: 'network-only'
+  }
 })(MessageContainer);
